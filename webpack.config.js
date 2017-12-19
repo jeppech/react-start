7f150d569a69bb60b0ae2c33393c097eb8e40690
Jeppe Christiansen <jeppe@codr.dk>
2017-12-19 22:35:44 +0100

    test conditional build, with DefinePlugin

--- webpack.config.js
+++ webpack.config.js
@@ -2,47 +2,53 @@ const webpack = require('webpack');
 const path = require('path');
 const plg = {
     CommonsChunk: webpack.optimize.CommonsChunkPlugin,
+    Define: webpack.DefinePlugin,
     CleanWebpack: require('clean-webpack-plugin'),
     ExtractTextWebpack: require('extract-text-webpack-plugin'),
     HtmlWebpack: require('html-webpack-plugin')
 }
 
-const config = {
-    entry: {
-        common: ['react','react-dom'],
-        app: './src/app.js',
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx$)/,
-                exclude: /node_modules/,
-                use: ['babel-loader']
-            },
-            {
-                test: /\.s?[ca]ss$/,
-                use: ['style-loader', 'css-loader', 'sass-loader'] //plg.ExtractTextWebpack.extract('css-loader')
-            }
-        ]
-    },
-    plugins: [
-        new plg.CleanWebpack(['dist']),
-        new plg.CommonsChunk({
-            minChunks: 3,
-            name: "common"
-        }),
-        //new plg.ExtractTextWebpack('styles.css'), // only for production, no HMR
-        new plg.HtmlWebpack({
-            template: 'src/index.html'
-        })
-    ],
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: '[name].js'
-    },
-    devServer: {
-        historyApiFallback: true
+module.exports = env => {
+    let BUILD_ENV = env ? env.NODE_ENV : 'development';
+
+    return {
+        entry: {
+            common: ['react','react-dom'],
+            app: './src/app.js',
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.(js|jsx$)/,
+                    exclude: /node_modules/,
+                    use: ['babel-loader']
+                },
+                {
+                    test: /\.s?[ca]ss$/,
+                    use: ['style-loader', 'css-loader', 'sass-loader'] //plg.ExtractTextWebpack.extract('css-loader')
+                }
+            ]
+        },
+        plugins: [
+            new plg.CleanWebpack(['dist']),
+            new plg.CommonsChunk({
+                minChunks: 3,
+                name: "common"
+            }),
+            new plg.Define({
+                'process.env.NODE_ENV': JSON.stringify(BUILD_ENV)
+            }),
+            //new plg.ExtractTextWebpack('styles.css'), // only for production, no HMR
+            new plg.HtmlWebpack({
+                template: 'src/index.html'
+            })
+        ],
+        output: {
+            path: path.resolve(__dirname, 'dist'),
+            filename: '[name].js'
+        },
+        devServer: {
+            historyApiFallback: true
+        }
     }
 }
-
-module.exports = config;
